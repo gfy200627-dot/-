09697c9ea9c0adec92fa8b91783fb0d97dad5b8f
gfy200627-dot <gfy200627@gmail.com>
2026-09-04 16:00:08 +0800

    feat(intro): mount AutoInsight opening animation

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
       <component :is="Component" />
     </transition>
   </router-view>
+
+  <IntroAnimation v-if="showIntro" @complete="showIntro = false" />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import IntroAnimation from '@/components/intro/IntroAnimation.vue'
+
 /**
  * 应用根组件
- * 仅负责路由视图切换与页面级淡入动画
+ * 负责路由视图切换，并在进入应用时播放一次品牌开场动画。
  */
+const showIntro = ref(true)
 </script>
